2e547e230475dcbba51aeaa110b121871b5d2675
Raphael Oracion <oracionr@student.douglascollege.ca>
2023-03-30 17:04:45 -0700

    fourth changes

--- server.js
+++ server.js
@@ -1,35 +1,63 @@
-const mongoose = require('mongoose');
-const express = require('express');
-const port = process.env.PORT || 3000;
-app = express();
+// const mongoose = require('mongoose');
+// const express = require('express');
+// const port = process.env.PORT || 3000;
+// app = express();
 
-mongoose.connect('mongodb+srv://raphael:<123>@cluster0.gmi5hux.mongodb.net/test')
+// mongoose.connect('mongodb+srv://raphael:<123>@cluster0.gmi5hux.mongodb.net/test')
 
-const activityschema = new mongoose.Schema({
-    activity: {type: String, required:true}
-})
-
-
-
-
-// app.get('/', (req,res) => {
-
-
+// const activityschema = new mongoose.Schema({
+//     activity: {type: String, required:true}
 // })
 
 
-app.get('/', (req,res)=> {
-    const task1 = new activitymodel({
-        activity: 'activity1'
 
-    })
 
-    activitymodel.insertmany([task1])
+// // app.get('/', (req,res) => {
 
-    res.send('<h2>Document Address<h2>')
 
-})
+// // })
 
+
+// app.get('/', (req,res)=> {
+//     const task1 = new activitymodel({
+//         activity: 'activity1'
+
+//     })
+
+//     activitymodel.insertmany([task1])
+
+//     res.send('<h2>Document Address<h2>')
+
+// })
+
+// app.listen(port, () => {
+//     console.log('server is running on port:' + port)
+// })
+const mongoose = require('mongoose');
+(express = require('express')), (app = express());
+const port = 3000;
+mongoose.connect('mongodb://127.0.0.1:27017/Activities', {
+useNewUrlParser: true,
+});
+// Create a Schema object
+const activitySchema = new mongoose.Schema({
+activity: { type: String, required: true },
+});
+// This Activitry creates the collection called activitimodels
+const Activitymodel = mongoose.model('Activity', activitySchema);
+app.get('/', (req, res) => {
+const task1 = new Activitymodel({
+activity: 'activity 1',
+});
+Activitymodel.insertMany([task1], function (err) {
+if (err) {
+console.log(err);
+} else {
+console.log('successfully created db');
+// mongoose.connection.close(); }
+});
+res.send(`<h1>Document Added</h1>`);
+});
 app.listen(port, () => {
-    console.log('server is running on port:' + port)
-})
+console.log(`Server is running on port: ${port}`);
+});
